aaf3e6700da71c2bf7c87191cad6be1bc81d7ed3
Dmitry Nischeta <dnishcheta@gmail.com>
2024-09-30 10:28:24 +0500

    Format code

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,26 +1,26 @@
 import {
-    isVue2,
-    shallowRef,
-    PluginObject,
-  } from "vue-demi";
-  import { TonConnectUI } from "@tonconnect/ui";
-  import { isClientSide } from "./utils/web";
-  import { TonConnectUIProviderProps } from "./types";
-  
-  export const TonConnectUIPlugin: PluginObject<TonConnectUIProviderProps> = {
-    install(app, options) {
-      const tonConnectUI = shallowRef<TonConnectUI | null>(null);
+  isVue2,
+  shallowRef,
+  PluginObject,
+} from "vue-demi";
+import { TonConnectUI } from "@tonconnect/ui";
+import { isClientSide } from "./utils/web";
+import { TonConnectUIProviderProps } from "./types";
 
-      // Initialize TonConnectUI if on client side
-      if (isClientSide() && !tonConnectUI.value) {
-        tonConnectUI.value = new TonConnectUI(options || {});
-      }
+export const TonConnectUIPlugin: PluginObject<TonConnectUIProviderProps> = {
+  install(app, options) {
+    const tonConnectUI = shallowRef<TonConnectUI | null>(null);
 
-      if (isVue2) {
-          app.prototype.$tonConnectUI = tonConnectUI.value;
-        } else {
-          // @ts-expect-error
-        app.config.globalProperties.$tonConnectUI = tonConnectUI.value;
-      }
-    },
+    // Initialize TonConnectUI if on client side
+    if (isClientSide() && !tonConnectUI.value) {
+      tonConnectUI.value = new TonConnectUI(options || {});
+    }
+
+    if (isVue2) {
+      app.prototype.$tonConnectUI = tonConnectUI.value;
+    } else {
+      // @ts-expect-error
+      app.config.globalProperties.$tonConnectUI = tonConnectUI.value;
+    }
+  },
 };
