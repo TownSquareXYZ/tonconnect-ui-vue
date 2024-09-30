f11346818006f522b5847ea3d965386a04a7223f
Dmitry Nischeta <dnishcheta@gmail.com>
2024-09-30 10:28:15 +0500

    Fix nuxt issue

--- src/hooks/useTonConnectUI.ts
+++ src/hooks/useTonConnectUI.ts
@@ -4,19 +4,19 @@ import { checkProvider } from '../utils/errors';
 import { isServerSide } from '../utils/web';
 
 export function useTonConnectUI() {
-    const globalPropertiesMap = isVue2
-        ? getCurrentInstance()?.proxy
-        // @ts-expect-error
-        : getCurrentInstance().appContext.app.config.globalProperties;
-
-    const tonConnectUI = globalPropertiesMap.$tonConnectUI;
-
     if (isServerSide()) {
         return {
             tonConnectUI: null as unknown as TonConnectUI,
             setOptions: () => {},
         };
     }
+
+    const globalPropertiesMap = isVue2
+        ? getCurrentInstance()?.proxy
+        // @ts-expect-error
+        : getCurrentInstance().appContext.app.config.globalProperties;
+
+    const tonConnectUI = globalPropertiesMap.$tonConnectUI;
 
     const setOptions = (options: TonConnectUiOptions) => {
         if (tonConnectUI && tonConnectUI) {
